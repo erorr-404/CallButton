447e92b8fa45dc18326be7015d09237f5dbd9990
Erorr <maxymholub@gmail.com>
2024-12-19 23:03:01 +0200

    Second day of developing

--- esp32/kitchen/main.py
+++ esp32/kitchen/main.py
@@ -8,13 +8,17 @@ from machine import Pin
 
 # Enable the Mini MP3 DFPlayer
 player = Player(pin_TX=17, pin_RX=16)
-player.volume(30)
+player.volume(10)
 player.module_sleep()
 
 led = Pin(2, Pin.OUT)
+pared_led = Pin(18, Pin.OUT)
 
 dinner_btn = Button(21)
 dinner_btn.set_debounce_time(100)  # Set debounce time to 100 milliseconds
+
+come_btn = Button(19)
+come_btn.set_debounce_time(100)  # Set debounce time to 100 milliseconds
 
 
 # Play sound and go sleep
@@ -44,19 +48,45 @@ sta.active(True)
 e = espnow.ESPNow()
 e.active(True)
 
+pared = False 
 peer = b'<\x8a\x1f\x9d\x1e\x1c'   # MAC address of peer's wifi interface
 e.add_peer(peer)      # Must add_peer() before send()
-e.send(peer, "pare", True)
+
+for i in range(30):
+    e.send(peer, "pare", True) # Ping the second ESP
+    print(f"Trying to pare: {i}...")
+    
+    host, msg = e.recv()
+    if msg: # msg == None if timeout in recv()
+        if msg == "accept": # if paring is successful
+            pared = True
+            pared_led.on()
+            print("Pare created!")
+            break
+    
+    sleep(1) # wait a second to save battery
 
 while True:
-    dinner_btn.loop()
-
+    dinner_btn.loop() # inform about loop
+    come_btn.loop() # inform about loop
+    
     if dinner_btn.is_pressed():
-        print("The button is pressed")
+        print("The dinner button is pressed")
         led.on()
 
     if dinner_btn.is_released():
-        print("The button is released")
+        print("The dinner button is released")
+    
+    if come_btn.is_pressed():
+        print("The come button is pressed")
         led.off()
 
+    if come_btn.is_released():
+        print("The come button is released")
+    
+    if pared:
+        pass
+    else:
+        break
+        
 e.send(peer, b'end', True)
--- esp32/room/main.py
+++ esp32/room/main.py
@@ -1,4 +1,5 @@
 from dfplayermini import Player
+from DIYables_MicroPython_Button import Button
 from time import sleep
 import network
 import espnow
@@ -12,6 +13,15 @@ player.module_sleep()
 
 
 led = Pin(2, Pin.OUT)
+
+accept_btn = Button(16)
+accept_btn.set_debounce_time(100)  # Set debounce time to 100 milliseconds
+
+wait_btn = Button(19)
+wait_btn.set_debounce_time(100)  # Set debounce time to 100 milliseconds
+
+reject_btn = Button(21)
+reject_btn.set_debounce_time(100)  # Set debounce time to 100 milliseconds
 
 
 # Play sound and go sleep
@@ -40,6 +50,24 @@ e.active(True)
 print(sta.config('mac').hex())
 print(sta.config('mac'))
 
+def wait_for_user_answear():
+    for i in range(15):
+        accept_btn.loop()
+        wait_btn.loop()
+        reject_btn.loop()
+        print(f"Waiting for user response: {i}...")
+        
+        # todo: make esp return a response
+        if accept_btn.is_pressed():
+            print("User accepted request") 
+        
+        if wait_btn.is_pressed():
+            print("User delayed request")
+        
+        if reject_btn.is_pressed():
+            print("User rejected request")
+
+pared = False 
 peer = None # peer mac
 
 while True:
@@ -47,16 +75,22 @@ while True:
     
     if msg: # msg == None if timeout in recv()
         
-        if msg == b'pare' # command to create peer for 2-way connection
+        if msg == b'pare': # command to create peer for 2-way connection
         print("Got pare command")
             if peer is None: # if it is the first message
                 peer = host # save mac address
                 e.add_peer(peer) # peer with device
+                e.send(peer, "accept", True) # return that paring is succesful
+                pared = True
+                led.on()
+                print("Successfully pared")
         
         elif msg == b'dinner':,
             print("Got dinner command")
             play_dinner_ready()
-            # todo: wait for user to press the button and return response
+            for i in range(15):
+                
+                # todo: wait for user to press the button and return response
         
         elif msg == b'come':
             print("Got come command")
